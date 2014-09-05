5ab86b9e441d03a488f74aadc7c41293093f0905
Michael Lu <michaellu@Michaels-MacBook-Pro.local>
2014-09-04 20:33:07 -0400

    first commit

---
+++ MyPlayground.playground/section-1.swift
@@ -0,0 +1,42 @@
+// Playground - noun: a place where people can play
+
+import UIKit
+
+var favoriteFruit: String = "Mango"
+
+var favoriteCookie: String = "Chocolate"
+
+
+var awesomeFriend: String = "Chelsie is awesome"
+
+var myAge: Int = 35
+
+var myNumber: Int = 9
+
+var mySection: Int = 11
+
+var myGPA: Float = 3.4
+
+var YouAreCool: Bool = true
+
+var str = "I told you so!"
+
+var thisYear: Int = 2010
+
+var worstSinger: String = "Justin Beiber"
+
+var IamProgramming: Bool = true
+
+var myFloat: Float = 35.555
+
+var myAwesomeWebsite: String = "www.michaellucv.com"
+
+
+
+
+
+
+
+
+
+
